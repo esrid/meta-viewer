9d452300da4b456e0df04f9125e33445f3e3855b
Alexandre Desir <alexandredesir@MacBook-Pro-de-Alexandre.local>
2024-06-20 16:36:28 -0400

    ajustement

--- logic/httplogic.go
+++ logic/httplogic.go
@@ -6,16 +6,19 @@ import (
 )
 
 const (
+	// 500 MG
 	maxSize int64 = 500 * 1024 * 1024
 )
 
 func FileUpload(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseMultipartForm(maxSize); err != nil {
+		fmt.Printf("multipart : %s", err)
 		http.Error(w, "Status Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	file, _, err := r.FormFile("file")
 	if err != nil {
+		fmt.Printf("file : %s", err)
 		http.Error(w, "Status Internal Server Error", http.StatusInternalServerError)
 		return
 	}
