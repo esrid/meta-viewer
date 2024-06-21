cb5b2cb55528c5937d3f2bcb421eaf44b7f8603b
keen-c <alexandre.desir@icloud.com>
2024-06-21 16:06:56 -0400

    oon

--- logic/exiftool.go
+++ logic/exiftool.go
@@ -12,6 +12,68 @@ const (
 	exifVersion string = "ExifTool Version Number"
 )
 
+var options = []string{
+	"-a",
+	"-b",
+	"-c",
+	"-charset",
+	"-csv",
+	"-d",
+	"-D",
+	"-e",
+	"-E",
+	"-ex",
+	"-ext",
+	"-f",
+	"-F",
+	"-fast",
+	"-fast2",
+	"-G",
+	"-g",
+	"-h",
+	"-H",
+	"-htmlDump",
+	"-i",
+	"-if",
+	"-k",
+	"-l",
+	"-lang",
+	"-L",
+	"-listItem",
+	"-listx",
+	"-m",
+	"-n",
+	"-p",
+	"-P",
+	"-password",
+	"-progress",
+	"-r",
+	"-S",
+	"-s",
+	"-sep",
+	"-short",
+	"-t",
+	"-T",
+	"-v",
+	"-w",
+	"-W",
+	"-x",
+	"-X",
+	"-z",
+	"-@",
+	"-globalTimeShift",
+	"-use",
+	"-restore_original",
+	"-delete_original",
+	"-api",
+	"-common_args",
+	"-config",
+	"-echo",
+	"-efile",
+	"-execute",
+	"-file",
+}
+
 func READATA(filepath string) (map[string]string, error) {
 	cmd := exec.Command("exiftool", fmt.Sprintf("%s", filepath))
 	var out, outerr bytes.Buffer
@@ -33,8 +95,10 @@ func READATA(filepath string) (map[string]string, error) {
 		if strings.TrimSpace(astr[0]) == exifVersion {
 			continue
 		}
+		key := strings.TrimSpace(astr[0])
+		value := strings.TrimSpace(astr[1])
 
-		outputmap[astr[0]] = astr[1]
+		outputmap[key] = value
 	}
 	return outputmap, nil
 }
